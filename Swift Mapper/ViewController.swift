f62791c3100f2a2160eb9753d92223bfc1c1ea74
belousov <antbelousov@gmail.com>
2015-07-27 23:39:01 +0300

    Initial Commit

---
+++ Swift Mapper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift Mapper
+//
+//  Created by qqqqq on 27/07/15.
+//  Copyright (c) 2015 Anton Belousov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
