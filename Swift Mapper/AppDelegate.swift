da6caace24526fc1f06892af4c9b6245c9c6e7e5
Anton Belousov <antbelousov@gmail.com>
2015-09-18 11:27:28 +0300

    minor changes

--- Swift Mapper/AppDelegate.swift
+++ Swift Mapper/AppDelegate.swift
@@ -32,7 +32,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
 
-        
 //        let k: TestClass? = ObjectCreator.sharedCreator.createFromDictionary([
 //            "number" : NSNumber(double: 100.500),
 //        
